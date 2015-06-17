2176b8f4503242178ca0e7d5c084c6c1fd80f291
Intern <intern@Candoris-Intern-MBP.local>
2015-06-17 10:50:11 -0400

    Changes as of 6/17/15

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,113 +1,88 @@
-var main = angular.module("main", ["ngRoute"]);
+var main = angular.module("main", ["ngRoute", "ngSanitize", "ui.select"]);
+
+main.filter('propsFilter', function() {
+  return function(items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      items.forEach(function(item) {
+        var itemMatches = false;
+
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  }
+});
 
 main.config(['$routeProvider', '$locationProvider', 
-	function($routeProvider, $locationProvider) {
+	function($routeProvider, uiSelectConfig, $locationProvider) {
 	$routeProvider.
 		when("/edit", {
 			templateUrl: "/public/edit.html",
 			controller: "editCtrl"
 		}).
-		when("/alt", {
-			templateUrl: "/public/alt.html",
-			controller: "altCtrl"
-		}).
 		when("/list", {
 			templateUrl: "/public/list.html",
 			controller: "listCtrl"
 		}).
-		when("/new", {
-			templateUrl: "/public/new.html",
-			controller: "newCtrl"
-		}).
 		otherwise({
 			redirectTo: "/list"
 		});
+
+		uiSelectConfig.theme = 'bootstrap';
+		//uiSelectConfig.resetSearchInput = true;
+		//uiSelectConfig.appendToBody = true;
 }]);
 
-main.controller('altCtrl', function($scope) {
-	$scope.message = 'This is the alternate page.';
-});
-
 main.controller("listCtrl", function($scope) {
+	$scope.notesTaken = [];
+	$scope.setNotesToUpload(false);
+
 	if(typeof Storage !== "undefined") {
-		console.log("Came to list");
 		if(localStorage.notesTaken) {
 			$scope.notesTaken = JSON.parse(localStorage.notesTaken);
-		} else {
-			$scope.notesTaken = [];
+			$scope.setNotesToUpload(true);
 		}
 	}
+
 });
 
-main.controller('editCtrl', function($scope, $location) {
+main.controller('editCtrl', function ($scope, $location, $http) {
 	var notesTaken = [];
-	
+	$scope.readyForEditing = false;
+	$scope.choosingTemplate = false;
+	$scope.templates = [];
+	$scope.oppChosen = null;
+	$scope.gettingTemplates = false;
+	$scope.opp = {
+		selected: null
+	}
+
 	$scope.editedNote = {
 		noteBody: undefined,
-		opportunityID: undefined	
-	};
-
-	function pullUpNote() {
-		var query = $location.search();
-		var index = query.index;
-
-		if(!index) {
-			$location.path("#/list");
-		} else {
-			notesTaken = JSON.parse(localStorage.notesTaken);
-			$scope.editedNote = notesTaken[index];
-		}
-	}
-
-	function saveNote() {
-		$scope.editedNote.dateModified = new Date();
-		localStorage.notesTaken = JSON.stringify(notesTaken);
-		$location.path("#/list");
-	}
-
-	$scope.saveNote = saveNote;
-
-	pullUpNote();
-});
-
-main.controller('newCtrl', function($scope, $location) {
-	$scope.choseTemplate = false;
-	$scope.newNote = {
-		noteBody: undefined,
 		opportunityID: undefined,
-		//dateModified: undefined
+		opportunityName: undefined
 	};
 
-	function zeroNewNote() {
-		$scope.newNote.noteBody = undefined;
-		$scope.newNote.opportunityID = undefined;
-		//$scope.newNote.dateModified = undefined;
-	}
-
-	function saveNote() {
-		var n, notesTaken;
-		if(typeof Storage !== "undefined") {
-			n = angular.copy($scope.newNote);
-			n.dateModified = new Date();
-			zeroNewNote();
-			if(localStorage.notesTaken) {
-				notesTaken = JSON.parse(localStorage.notesTaken);
-			} else {
-				notesTaken = [];
-			}
-			notesTaken.push(n);
-			localStorage.notesTaken = JSON.stringify(notesTaken);
-			$scope.$parent.notesToUpload = true;
-		} else {
-
-		}
-		$location.path("#/list");
-	}
-
-	$scope.saveNote = saveNote;
-
-
-	function pullTemplatesFromStorage() {
+	function getTemplatesFromStorage() {
+		$scope.gettingTemplates = true;
 		if(typeof Storage !== "undefined") {
 			if(localStorage.templates) {
 				$scope.templates = JSON.parse(localStorage.templates);
@@ -115,30 +90,149 @@ main.controller('newCtrl', function($scope, $location) {
 				$scope.templates = [];
 			}
 		}
+		$scope.gettingTemplates = false;
 	}
 
-	function startNoteFromTemplate(template) {
-		$scope.newNote.noteBody = template.templateBody;
-		$scope.choseTemplate = true;
+	$scope.getTemplatesFromSalesforce = function() {
+		$scope.gettingTemplates = true;
+		$http.get("/api/templates").
+			success(function(data, status, headers, config) {
+				localStorage.templates = JSON.stringify(data);
+				$scope.templates = data;
+				$scope.gettingTemplates = false;
+			}).error(function() {
+				$scope.gettingTemplates = false;
+			});
 	}
 
-	$scope.startNoteFromTemplate = startNoteFromTemplate;
+	$scope.startNoteFromTemplate = function(template) {
+		$scope.editedNote.noteBody = template.templateBody;
+		$scope.choosingTemplate = false;
+		$scope.readyForEditing = true;
+	}
 
-	pullTemplatesFromStorage();
+	function setUpNote() {
+		var query = $location.search();
+		var id = query.id;
+		var opps;
+
+		if(!id) {
+			$location.path("#/list");
+
+		} else if(id === "new") {
+
+			if(localStorage.notesTaken) {
+				notesTaken = JSON.parse(localStorage.notesTaken);
+			} else {
+				notesTaken = [];
+			}
+			
+			notesTaken.push($scope.editedNote);
+			$scope.editedNote = notesTaken[notesTaken.length - 1];
+
+			getTemplatesFromStorage();
+			$scope.choosingTemplate = true;
+
+		} else {
+			notesTaken = JSON.parse(localStorage.notesTaken);
+			$scope.editedNote = notesTaken[id];
+			opps = $scope.getOpps();
+			for(var i = 0; i < opps.length; i++) {
+				if(opps[i].Id === $scope.editedNote.opportunityID) {
+					$scope.opp.selected = opps[i];
+					break;
+				}
+			}
+
+			$scope.readyForEditing = true;
+		}
+	}
+
+	$scope.saveNote = function() {
+		$scope.editedNote.dateModified = moment().format('MMMM Do YYYY, h:mm:ss a');
+		localStorage.notesTaken = JSON.stringify(notesTaken);
+		$location.path("#/list");
+	}
+
+	function pullOppsFromStorage() {
+		if(typeof Storage !== "undefined" && localStorage.opps) {
+			$scope.setOpps(JSON.parse(localStorage.opps));
+		}
+		else {
+			$scope.setOpps([]);
+		}
+		$scope.setGettingOpps(false);
+	}
+
+	$scope.getOppsFromSalesforce = function () {
+		$scope.setGettingOpps("Fetching Opportunities");
+		$http.get("/api/salesforce/opportunity").
+			success(function(data, status, headers, config) {
+				$scope.setOpps(data);
+				localStorage.opps = JSON.stringify(data);
+				$scope.setGettingOpps(false);
+			}).error(function() {
+				//$scope.setGettingOpps("Fetch Failed");
+			});
+	}
+
+	$scope.selectOpp = function (item, model) {
+		$scope.editedNote.opportunityID = item.Id;
+		$scope.editedNote.opportunityName = item.Name;
+	}
+	
+
+	if($scope.getGettingOpps()) {
+		pullOppsFromStorage();
+	}
+	setUpNote();
+
 });
 
+
 main.controller("mainCtrl", function ($scope, $http) {
+	$scope.notesToUpload = false;
+
+	$scope.gettingOpps = "Fetching Opportunities";
+	$scope.opps = [];
+
+	$scope.newNote = {
+		noteBody: undefined,
+		opportunityID: undefined,
+			//dateModified: undefined
+		};
+
 		$scope.connected = false;
 		$scope.message = "";
 		$scope.hasMessage = false;
-		$scope.templates = [];
 
-		
-		$scope.notesToUpload = (localStorage.notesTaken ? true : false);
+		$scope.getConnected = function () {
+			return $scope.connected;
+		}
 
-		function uploadNotes () {
+		$scope.setNotesToUpload = function(tf) {
+			$scope.notesToUpload = tf;
+		}
+
+		$scope.setGettingOpps = function (go) {
+			$scope.gettingOpps = go;
+		}
+
+		$scope.getGettingOpps = function () {
+			return $scope.gettingOpps;
+		}
+
+		$scope.setOpps = function (o) {
+			$scope.opps = o;
+		}
+
+		$scope.getOpps = function () {
+			return $scope.opps;
+		}
+
+		$scope.uploadNotes = function () {
 			$http.post("/api/upload", localStorage.notesTaken).
-				success(function(data, status, headers, config) {
+			success(function(data, status, headers, config) {
 					$scope.message = "Data Sent";
 					$scope.hasMessage = true;
 					localStorage.removeItem("notesTaken");
@@ -149,33 +243,16 @@ main.controller("mainCtrl", function ($scope, $http) {
 					$scope.notesToUpload = true;
 				});
 		}
-
-		$scope.uploadNotes = uploadNotes;
 		
 		function checkConnection () {
-			$http.get("/public/check").
-				success(function(data, status, headers, config) {
+			$http.get("/api/check").
+			success(function(data, status, headers, config) {
 					$scope.connected = true;
 				}).error(function(data, status, headers, config) {
 					$scope.connected = false;
 				});
 		}
 
-		function getTemplates() {
-			$http.get("/api/templates").
-				success(function(data, status, headers, config) {
-					localStorage.templates = JSON.stringify(data);
-					$scope.templates = data;
-					$scope.hasMessage = true;
-					$scope.message = "Got Templates";
-				}).error(function() {
-					$scope.hasMessage = true;
-					$scope.message = "Failed To Obtain Templates";
-				});
-		}
-
-		$scope.getTemplates = getTemplates;
-		$scope.checkConnection = checkConnection;
-
-		checkConnection();
+		checkConnection(); //because setInterval waits 10 secs to get started
+		var checkIntervalId = window.setInterval(checkConnection, 10000);
 	});
